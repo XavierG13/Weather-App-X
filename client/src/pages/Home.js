5c05a3354b29421d6ef2f4c0bef9fe396cd2a48d
Creature <gzmnxavi112@gmaIL.COM>
2021-05-29 21:11:24 -0500

    created home page, set up latitude and longitude

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function Home() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is ", lat);
+    console.log("Longitude is ", long);
+  }, [lat, long]);
+
+  return <div className="coordinates"></div>;
+}
+
+export default Home;
